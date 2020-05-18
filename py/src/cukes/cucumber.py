f18c55b33ee6092c38bd00db1f71f256820ff32c
Gaming32 <31409929+Gaming32@users.noreply.github.com>
2020-05-18 16:18:02 -0500

    add thread locking

--- py/src/cukes/cucumber.py
+++ py/src/cukes/cucumber.py
@@ -7,22 +7,26 @@ from .internal_constants import Commands
 
 class CucumberClient(_CucumberMixin):
     def __init__(self, address_family=socket.AF_INET):
+        self.mutex = _thread.allocate_lock()
         self.sock = socket.socket(address_family)
 
     def _handshake(self):
-        self._send_chunks(Commands.initiate_handshake)
-        done_check = self._recv_exact(2)
-        if done_check != Commands.handshake_complete:
-            raise InvalidDataError('invalid handshake end: %r' % done_check)
+        with self.mutex:
+            self._send_chunks(Commands.initiate_handshake)
+            done_check = self._recv_exact(2)
+            if done_check != Commands.handshake_complete:
+                raise InvalidDataError('invalid handshake end: %r' % done_check)
 
     def connect(self, host='localhost', port=28537, address=None):
         if address is None:
             address = (host, port)
-        self.sock.connect(address)
+        with self.mutex:
+            self.sock.connect(address)
         self._handshake()
 
     def get_nodes(self):
-        pass
+        with self.mutex:
+            self._send_chunks(Commands.get_nodes)
 
 
 __all__ = ['CucumberClient']
